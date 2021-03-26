4f8df24a26d808ecad7ecc6604c6ab8cf14d794b
Akshay <araa@connect.ust.hk>
2021-03-26 14:02:30 +0800

    Update test file names

--- dataflow/run-local.sh
+++ dataflow/run-local.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-mvn -Pdirect-runner compile exec:java \
--Dexec.mainClass=io.blockchainetl.matic.maticPubSubToBigQueryPipeline \
--Dexec.args="--chainConfigFile=chainConfigMaticDev.json \
---tempLocation=gs://matic-etl-dev-dataflow-0/ \
---outputErrorsTable=mainnet.errors"
+# mvn -Pdirect-runner compile exec:java \
+# -Dexec.mainClass=io.blockchainetl.matic.MaticPubSubToBigQueryPipeline \
+# -Dexec.args="--chainConfigFile=chainConfig.json \
+# --tempLocation=gs://matic-etl-dev-dataflow-0/ \
+# --outputErrorsTable=mainnet.errors" 
+# -X
 
+mvn -Pdirect-runner compile exec:java \
+-Dexec.mainClass=io.blockchainetl.matic.MaticPubSubToBigQueryPipeline \
+-Dexec.args="--chainConfigFile=chainConfig.json --allowedTimestampSkewSeconds=36000 --defaultSdkHarnessLogLevel=DEBUG \
+--tempLocation=gs://nansen-dev/dataflow"
