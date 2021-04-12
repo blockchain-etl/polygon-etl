d71eb708b8a56cedfef07748177cce1d4c17e40d
medvedev1088 <medvedev1088@gmail.com>
2021-04-13 01:38:25 +0700

    Bump version

--- cli/maticetl/cli/__init__.py
+++ cli/maticetl/cli/__init__.py
@@ -43,7 +43,7 @@ from maticetl.cli.stream import stream
 
 
 @click.group()
-@click.version_option(version='0.0.3')
+@click.version_option(version='0.0.4')
 @click.pass_context
 def cli(ctx):
     pass
