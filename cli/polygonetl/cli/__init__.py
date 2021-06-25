3a97641b91edd459920a906d06c628117c3388f0
medvedev1088 <medvedev1088@gmail.com>
2021-06-25 21:22:58 +0700

    Bump version

--- cli/polygonetl/cli/__init__.py
+++ cli/polygonetl/cli/__init__.py
@@ -43,7 +43,7 @@ from polygonetl.cli.stream import stream
 
 
 @click.group()
-@click.version_option(version='0.1.2')
+@click.version_option(version='0.1.3')
 @click.pass_context
 def cli(ctx):
     pass
