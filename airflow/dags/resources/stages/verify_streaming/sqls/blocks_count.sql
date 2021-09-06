a000186b33e0e3abb5d2e5a728646f52f4c744f3
medvedev1088 <medvedev1088@gmail.com>
2021-09-06 17:25:32 +0800

    Increase threshold for duplicate blocks in verify_streaming dag

--- airflow/dags/resources/stages/verify_streaming/sqls/blocks_count.sql
+++ airflow/dags/resources/stages/verify_streaming/sqls/blocks_count.sql
@@ -3,5 +3,5 @@ select if(
   select count(*) - (max(number) - min(number) + 1)
   from `{{params.destination_dataset_project_id}}.{{params.dataset_name}}.blocks` as blocks
   where date(timestamp) >= date_add('{{ds}}', INTERVAL -1 DAY)
-) between -2 and 2, 1,
-cast((select 'There are more than 2 missing or duplicate blocks') as INT64))
+) between -4 and 4, 1,
+cast((select 'There are more than 4 missing or duplicate blocks') as INT64))
