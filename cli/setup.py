c832da4214b2d24a2eca50a62fcbfbdac6223592
charlielewisme <cha.lewis@gmail.com>
2022-06-07 16:43:24 +0100

    format

--- cli/setup.py
+++ cli/setup.py
@@ -7,62 +7,62 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-long_description = read('README.md') if os.path.isfile("README.md") else ""
+long_description = read("README.md") if os.path.isfile("README.md") else ""
 
 setup(
-    name='polygon-etl',
-    version='0.1.8',
-    author='Evgeny Medvedev',
-    author_email='evge.medvedev@gmail.com',
-    description='Tools for exporting Polygon blockchain data to CSV or JSON',
+    name="polygon-etl",
+    version="0.1.8",
+    author="Evgeny Medvedev",
+    author_email="evge.medvedev@gmail.com",
+    description="Tools for exporting Polygon blockchain data to CSV or JSON",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/blockchain-etl/polygon-etl',
-    packages=find_packages(exclude=['schemas', 'tests']),
+    long_description_content_type="text/markdown",
+    url="https://github.com/blockchain-etl/polygon-etl",
+    packages=find_packages(exclude=["schemas", "tests"]),
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords='polygon',
+    keywords="polygon",
     # web3.py doesn't work on 3.5.2 and less (https://github.com/ethereum/web3.py/issues/1012)
-    python_requires='>=3.5.3,<4',
+    python_requires=">=3.5.3,<4",
     install_requires=[
-        'web3==4.7.2',
-        'eth-utils==1.8.4',
-        'eth-abi==1.3.0',
-        'click==7.0',
-        'ethereum-dasm==0.1.4',
-        'base58',
-        'requests',
-        'blockchain-etl-common==1.6.1',
+        "base58",
+        "blockchain-etl-common==1.6.1",
+        "click==7.0",
+        "eth-abi==1.3.0",
+        "eth-utils==1.8.4",
+        "ethereum-dasm==0.1.4",
+        "requests",
+        "web3==4.7.2",
     ],
     extras_require={
-        'streaming': [
-            'timeout-decorator==0.4.1',
-            'google-cloud-pubsub==2.1.0',
-            'google-cloud-storage==1.33.0',
-            'sqlalchemy==1.3.13',
-            'pg8000==1.13.2',
+        "streaming": [
+            "google-cloud-pubsub==2.1.0",
+            "google-cloud-storage==1.33.0",
+            "pg8000==1.13.2",
+            "sqlalchemy==1.3.13",
+            "timeout-decorator==0.4.1",
         ],
-        'dev': [
-            'pytest==7.1.2', 'black==20.8b0', 'isort==5.10.1',
-        ]
+        "dev": [
+            "black==20.8b0",
+            "isort==5.10.1",
+            "pytest==7.1.2",
+        ],
     },
     entry_points={
-        'console_scripts': [
-            'polygonetl=polygonetl.cli:cli',
+        "console_scripts": [
+            "polygonetl=polygonetl.cli:cli",
         ],
     },
     project_urls={
-        'Bug Reports': 'https://github.com/blockchain-etl/polygon-etl/issues',
-        'Source': 'https://github.com/blockchain-etl/polygon-etl',
+        "Bug Reports": "https://github.com/blockchain-etl/polygon-etl/issues",
+        "Source": "https://github.com/blockchain-etl/polygon-etl",
     },
 )
-
-
