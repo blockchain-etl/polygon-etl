570f69d16904e7cb0a83376dea4cafa90609f0da
medvedev1088 <medvedev1088@gmail.com>
2023-10-05 14:28:21 +0100

    Validate bytecode in EthContractService.get_function_sighashes

--- cli/polygonetl/service/eth_contract_service.py
+++ cli/polygonetl/service/eth_contract_service.py
@@ -27,6 +27,9 @@ from ethereum_dasm.evmdasm import EvmCode, Contract
 class EthContractService:
 
     def get_function_sighashes(self, bytecode):
+        if not bytecode:
+            return []
+
         bytecode = clean_bytecode(bytecode)
         if bytecode is not None:
             evm_code = EvmCode(contract=Contract(bytecode=bytecode), static_analysis=False, dynamic_analysis=False)
